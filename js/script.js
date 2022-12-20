355a2e8f6ef6d2053b9fd4bc5b897a2642a432f4
MPReportCard <42910337+MPReportCard@users.noreply.github.com>
2022-12-20 14:39:30 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -72,10 +72,10 @@ function download_image() {
   link.click();
 }
 
-let issuesFor = ['Voted for an emergency budget to tackle the cost of living crisis.', 'Voted against Boris Johnson in a confidence vote.', 'Voted to save jobs at P&O Ferries.', 'Voted to ban fracking.', 'Voted to remove VAT from energy bills.']
-let issuesAgainst = ['Voted against an emergency budget to tackle the cost of living crisis.', 'Voted to have confidence in Boris Johnson.', 'Voted against saving jobs at P&O Ferries.', 'Voted to allow fracking.', 'Voted against removing VAT from energy bills.']
-let issuesNeutralLab = ['Was absent for the vote on an emergency budget.', 'Was absent for the confidence vote on Boris Johnson.', 'Was absent from the vote on P&O Ferries jobs.', 'Was absent for the vote on fracking.', 'Was absent for the vote on removing VAT from energy bills.']
-let issuesNeutralCon = ['Did not vote for an emergency budget to tackle the cost of living crisis.', 'Did not vote to remove Boris Johnson from office.', 'Did not vote to save jobs at P&O Ferries.', 'Did not vote to ban fracking.', 'Did not vote remove VAT from energy bills.']
+let issuesFor = ['Voted for an emergency budget to tackle the cost of living crisis', 'Voted against Boris Johnson in a confidence vote', 'Voted to save jobs at P&O Ferries', 'Voted to ban fracking', 'Voted to remove VAT from energy bills']
+let issuesAgainst = ['Voted against an emergency budget to tackle the cost of living crisis', 'Voted to have confidence in Boris Johnson', 'Voted against saving jobs at P&O Ferries', 'Voted to allow fracking', 'Voted against removing VAT from energy bills']
+let issuesNeutralLab = ['Was absent for the vote on an emergency budget', 'Was absent for the confidence vote on Boris Johnson', 'Was absent from the vote on P&O Ferries jobs', 'Was absent for the vote on fracking', 'Was absent for the vote on removing VAT from energy bills']
+let issuesNeutralCon = ['Did not vote for an emergency budget to tackle the cost of living crisis', 'Did not vote to remove Boris Johnson from office', 'Did not vote to save jobs at P&O Ferries', 'Did not vote to ban fracking', 'Did not vote remove VAT from energy bills']
 let forImage = ['img/A.png', 'img/A.png', 'img/A.png', 'img/A.png', 'img/A.png'];
 let againstImage = ['img/F.png', 'img/F.png', 'img/F.png', 'img/F.png', 'img/F.png'];
 //Sewage, Energy VAT, P&O Ferries, Fracking,
@@ -190,67 +190,52 @@ function checkVote(code, constituencyString) {
             document.getElementById(`issue1Name`).innerHTML = `Voted for an emergency budget to tackle the cost of living crisis.`;
             document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
             document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome1: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Voted against Boris Johnson in a confidence vote.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="Against"/>`;
-            console.log(`voteOutcome2: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Voted to end non-dom tax status.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome3: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Voted to ban fracking.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome4: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Voted to remove VAT from energy bills.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome5: Aye`);
+            document.getElementById(`issue2Name`).innerHTML = `Voted against Boris Johnson in a confidence vote.`;
+            document.getElementById(`absenceDisclaimer2`).innerHTML = ``;
+            document.getElementById(`voteOutcome2`).innerHTML = `<img src="img/A.png" width = 48px title="Against"/>`;
+            document.getElementById(`issue3Name`).innerHTML = `Voted to end non-dom tax status.`;
+            document.getElementById(`absenceDisclaimer3`).innerHTML = ``;
+            document.getElementById(`voteOutcome3`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
+            document.getElementById(`issue4Name`).innerHTML = `Voted to ban fracking.`;
+            document.getElementById(`absenceDisclaimer4`).innerHTML = ``;
+            document.getElementById(`voteOutcome4`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
+            document.getElementById(`issue5Name`).innerHTML = `Voted to remove VAT from energy bills.`;
+            document.getElementById(`absenceDisclaimer5`).innerHTML = ``;
+            document.getElementById(`voteOutcome5`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
           } else if (constituencyString == "Mitcham and Morden") {
 
           } else if (constituencyString == "Alyn and Deeside") {
             document.getElementById(`issue1Name`).innerHTML = `Voted for an emergency budget to tackle the cost of living crisis.`;
             document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
             document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome1: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Voted against a tax cut for bankers.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="Against"/>`;
-            console.log(`voteOutcome2: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Was a teller on the vote to save jobs at P&O Ferries.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome3: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Voted to ban fracking.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome4: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Voted to remove VAT from energy bills.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome5: Aye`);
+            document.getElementById(`issue2Name`).innerHTML = `Voted against a tax cut for bankers.`;
+            document.getElementById(`absenceDisclaimer2`).innerHTML = ``;
+            document.getElementById(`voteOutcome2`).innerHTML = `<img src="img/A.png" width = 48px title="Against"/>`;
+            document.getElementById(`issue3Name`).innerHTML = `Was a teller on the vote to save jobs at P&O Ferries.`;
+            document.getElementById(`absenceDisclaimer3`).innerHTML = ``;
+            document.getElementById(`voteOutcome3`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
+            document.getElementById(`issue4Name`).innerHTML = `Voted to ban fracking.`;
+            document.getElementById(`absenceDisclaimer4`).innerHTML = ``;
+            document.getElementById(`voteOutcome4`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
+            document.getElementById(`issue5Name`).innerHTML = `Voted to remove VAT from energy bills.`;
+            document.getElementById(`absenceDisclaimer5`).innerHTML = ``;
+            document.getElementById(`voteOutcome5`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
           } else if (constituencyString == "Nottingham South") {
             document.getElementById(`issue1Name`).innerHTML = `Voted for an emergency budget to tackle the cost of living crisis.`;
             document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
             document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome1: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Voted against a tax cut for bankers.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="Against"/>`;
-            console.log(`voteOutcome2: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Was a teller on the vote to save jobs at P&O Ferries.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome3: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Voted to ban fracking.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome4: Aye`);
-            document.getElementById(`issue1Name`).innerHTML = `Voted to remove VAT from energy bills.`;
-            document.getElementById(`absenceDisclaimer1`).innerHTML = ``;
-            document.getElementById(`voteOutcome1`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
-            console.log(`voteOutcome5: Aye`);
+            document.getElementById(`issue2Name`).innerHTML = `Voted against a tax cut for bankers.`;
+            document.getElementById(`absenceDisclaimer2`).innerHTML = ``;
+            document.getElementById(`voteOutcome2`).innerHTML = `<img src="img/A.png" width = 48px title="Against"/>`;
+            document.getElementById(`issue3Name`).innerHTML = `Was a teller on the vote to save jobs at P&O Ferries.`;
+            document.getElementById(`absenceDisclaimer3`).innerHTML = ``;
+            document.getElementById(`voteOutcome3`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
+            document.getElementById(`issue4Name`).innerHTML = `Voted to ban fracking.`;
+            document.getElementById(`absenceDisclaimer4`).innerHTML = ``;
+            document.getElementById(`voteOutcome4`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
+            document.getElementById(`issue5Name`).innerHTML = `Voted to remove VAT from energy bills.`;
+            document.getElementById(`absenceDisclaimer5`).innerHTML = ``;
+            document.getElementById(`voteOutcome5`).innerHTML = `<img src="img/A.png" width = 48px title="For"/>`;
           } else {
 
           for (let k = 0; k < ayeTellersArray.length; k++) {
